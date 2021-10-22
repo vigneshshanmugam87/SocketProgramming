afbe11d926c032cb319ded4edc2cf81b3037b8c5
vigneshshanmugam87 <92991689+vigneshshanmugam87@users.noreply.github.com>
2021-10-22 15:56:23 -0400

    Update MultiClientTcpServerSocket.cpp
    Added header files and enum defs

--- MultiClientTcpServerSocket.cpp
+++ MultiClientTcpServerSocket.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+enum TLV_TYPE
+{
+    GOODBYE = 0x0B1E,
+    DATA = 0xDA7A,
+    HELLO = 0xE110
+};
+
 struct ClientSockInfo
 {
 	int sd;
